593fc7db9698710cfd3dfa7599d8ff58c842985e
Alex Biba <alexbiba@gmail.com>
2022-09-11 14:55:47 +0300

    Add more alphabets, wip

--- src/password_generator/__main__.py
+++ src/password_generator/__main__.py
@@ -2,20 +2,53 @@
 
 import argparse
 import sys
-from .password_generator import PasswordGenerator
+import secrets
+import string
+
+class PasswordGenerator:
+    def __init__(self, length=8, digits=False, chars=False, uppercase=False, lowercase=False, symbols=False, hexadecimal=False, octal=False, printable=False):
+        self.length = length
+        self.chars = chars
+        self.digits = digits
+        self.uppercase = uppercase
+        self.lowercase = lowercase
+        self.symbols = symbols
+        self.hexadecimal = hexadecimal
+        self.octal = octal
+        self.printable = printable
+
+    def gen(self):
+        alphabet = ''
+        if (self.chars): alphabet += string.ascii_letters
+        if (self.digits): alphabet += string.digits
+        if (self.uppercase): alphabet += string.uppercase
+        if (self.lowercase): alphabet += string.lowercase
+        if (self.symbols): alphabet += string.symbols
+        if (self.hexadecimal): alphabet += string.hexadecimal
+        if (self.octal): alphabet += string.octal
+        if (self.printable): alphabet += string.printable
+        return ''.join(secrets.choice(alphabet) for i in range(self.length))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-l", "--length", type=int, default=8, help="specify the length of the password")
+    parser.add_argument("--length", type=int, default=8, help="specify the length of the password")
     parser.add_argument("-d", "--digits", action="store_true", help="use digits")
-    parser.add_argument("-c", "--chars", action="store_true", help="use alphanumeric ascii chars")
-    # parser.add_argument("-c", "--clipboard", default=False, help="copy result to clipboard")
+    parser.add_argument("-c", "--chars", action="store_true", help="use alphanumeric ascii chars (uppercase and lowercase)")
+    parser.add_argument("-u", "--uppercase", action="store_true", help="use uppercase ascii chars")
+    parser.add_argument("-l", "--lowercase", action="store_true", help="use lowercase ascii chars")
+    parser.add_argument("-s", "--symbols", action="store_true", help="use punctuation chars")
+    parser.add_argument("-x", "--hex", action="store_true", help="use hexadecimal")
+    parser.add_argument("-o", "--oct", action="store_true", help="use punctuation")
+    parser.add_argument("-p", "--printable", action="store_true", help="use ascii chars considered printable")
+
     args = parser.parse_args()
 
-    if (args.digits is False and args.chars is False):
-        print("One of -d/--digits or -c/--chars must be specified")
-        sys.exit(1)
-
-    password_generator = PasswordGenerator(args.length, args.digits, args.chars)
+    password_generator = PasswordGenerator(
+        args.length,
+        args.digits,
+        args.chars,
+        args.uppercase,
+        args.lowercase
+    )
     password = password_generator.gen()
     print(password)
